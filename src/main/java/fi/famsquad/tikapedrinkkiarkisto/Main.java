1a39ea83c11f6ae927a50d6cae1a3b93c9d07879
mattiost <mattiost@lx7-fuxi061.ad.helsinki.fi>
2017-10-19 11:50:41 +0300

    Index.html säätöä (korjaus)

--- src/main/java/fi/famsquad/tikapedrinkkiarkisto/Main.java
+++ src/main/java/fi/famsquad/tikapedrinkkiarkisto/Main.java
@@ -14,7 +14,7 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Tietokanta tietokanta = new Tietokanta("drinkkitietokanta.db");
+        Tietokanta tietokanta = new Tietokanta("db/drinkkitietokanta.db");
         DrinkkiDao dDao = new DrinkkiDao(tietokanta);
         Verkkosivu verkkosivu = new Verkkosivu(dDao);
     }
